ba79723abc17a335c5a4617911b2296b78f4dcab
Bernardo <programming@wandrell.com>
2019-06-14 15:13:20 +0200

    Updated redux-saga

--- src/store/configureStore.dev.js
+++ src/store/configureStore.dev.js
@@ -1,11 +1,12 @@
 import { createStore, applyMiddleware, compose } from 'redux';
 import createSagaMiddleware from 'redux-saga';
+import sagaMonitor from '@redux-saga/simple-saga-monitor';
 import { createLogger } from 'redux-logger';
 import rootReducer from 'reducers';
 import DevTools from 'development/components/DevTools';
 import rootSaga from 'sagas';
 
-const sagaMiddleware = createSagaMiddleware();
+const sagaMiddleware = createSagaMiddleware(sagaMonitor);
 
 /**
  * Development application middleware.
