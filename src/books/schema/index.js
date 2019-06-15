073aa9855818e51b0e5b806bceb97831caaac430
Bernardo <programming@wandrell.com>
2019-06-15 02:10:42 +0200

    Reduced schema

--- src/books/schema/index.js
+++ src/books/schema/index.js
@@ -1,27 +1,8 @@
 import { schema } from 'normalizr';
 
 /**
- * Author key schema.
- */
-export const authorKey = new schema.Entity('authorKeys');
-
-/**
- * Language schema.
- */
-export const language = new schema.Entity('languages');
-
-/**
- * Subject schema.
- */
-export const subject = new schema.Entity('subjects');
-
-/**
  * Document schema.
  */
-export const book = new schema.Entity('books', {
-   author_key: [authorKey],
-   language: [language],
-   subject: [subject]
-}, {
+export const book = new schema.Entity('books', {}, {
    idAttribute: 'key'
 });
