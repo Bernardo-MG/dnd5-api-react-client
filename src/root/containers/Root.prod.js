a8451a39c47f3bb0b04cbdef47dd2233fbbbd6df
Bernardo <programming@wandrell.com>
2019-06-25 22:19:04 +0200

    Reduced i18n configuration

--- src/root/containers/Root.prod.js
+++ src/root/containers/Root.prod.js
@@ -6,30 +6,20 @@ import { Provider } from 'react-redux';
 import routes from 'routes';
 import { HashRouter as Router } from 'react-router-dom';
 
-import { addLocaleData, IntlProvider } from 'react-intl';
-import en from 'react-intl/locale-data/en';
+import { IntlProvider } from 'react-intl';
 
 import localeData from 'i18n/messages.json';
-
-addLocaleData(en);
 
 const language = (navigator.languages && navigator.languages[0])
    || navigator.language
    || navigator.userLanguage;
-
-const languageWithoutRegionCode = language.toLowerCase().split(/[_-]+/)[0];
-
-// Try full locale, try locale without region code, fallback to 'en'
-const messages = localeData[languageWithoutRegionCode]
-   || localeData[language]
-   || localeData.en;
 
 /**
  * Production root application.
  */
 const Root = ({ store }) =>
    (
-      <IntlProvider locale={language} messages={messages}>
+      <IntlProvider locale={language} messages={localeData}>
          <Provider store={store}>
             <Router>
                {routes}
