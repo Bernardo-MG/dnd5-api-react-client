69dd9345e64ca833882848a9feb803c0d622bf65
Bernardo <programming@wandrell.com>
2019-07-14 13:05:04 +0200

    Format corrections

--- webpack.common.js
+++ webpack.common.js
@@ -42,22 +42,22 @@ module.exports = {
    },
    output: {
       path: path.join(__dirname, OUTPUT_PATH),
-      filename: "[name].js"
+      filename: '[name].js'
    },
    cache : true,
    optimization: {
       splitChunks: {
          cacheGroups: {
             commons: {
-               chunks: "initial",
+               chunks: 'initial',
                minChunks: 2,
                maxInitialRequests: 5, // The default limit is too small to showcase the effect
                minSize: 0 // This is example is too small to create commons chunks
             },
             vendor: {
                test: /node_modules/,
-               chunks: "initial",
-               name: "vendor",
+               chunks: 'initial',
+               name: 'vendor',
                priority: 10,
                enforce: true
             }
