1a4af67c6b9a0b1a4b51bde86ac83b15cd25d178
Bernardo <programming@wandrell.com>
2019-07-21 19:47:38 +0200

    Moved layout to its own package

--- src/views/index.js
+++ src/views/index.js
@@ -1,5 +1,4 @@
 import Index from 'views/components/Index';
 import SearchView from 'views/containers/SearchView';
-import DefaultSideMenuLayout from 'views/components/DefaultSideMenuLayout';
 
-export { Index, SearchView, DefaultSideMenuLayout };
+export { SearchView, Index };
